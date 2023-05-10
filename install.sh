a85bb2ff0dc88fc61f3741181b91a0dad87c76e7
Sam Tripp <1519984+sbt92@users.noreply.github.com>
2023-05-10 09:47:03 +0100

    Add pyenv config to .profile and .zprofile

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 ln -fs $(pwd)/gitconfig $HOME/.gitconfig
 ln -fs $(pwd)/vimrc $HOME/.vimrc
 ln -fs $(pwd)/zshrc $HOME/.zshrc
+ln -fs $(pwd)/profile $HOME/.profile
+ln -fs $(pwd)/zprofile $HOME/.zprofile
